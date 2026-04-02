313f6f598c6bd013078bf0d849386d5c278c80c3
juniorprotus <protuz2025@gmail.com>
2026-04-02 04:55:38 +0300

    refactor: remove unused and deprecated files

--- academia 2/backend/utils/db.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const path = require('path');
-require('dotenv').config();
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-
-
-
-module.exports = supabase;
